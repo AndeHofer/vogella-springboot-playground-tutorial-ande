ccffd8bdba660f842bb3b7d17fb7336691e1e9f1
AndeHofer <andehofer@gmail.com>
2021-12-27 18:36:11 +0100

    initial commit

---
+++ src/main/java/at/ande/vogella/springboot/playground/tutorial/VogellaSpringbootPlaygroundTutorialAndeApplication.java
@@ -0,0 +1,13 @@
+package at.ande.vogella.springboot.playground.tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VogellaSpringbootPlaygroundTutorialAndeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VogellaSpringbootPlaygroundTutorialAndeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/at/ande/vogella/springboot/playground/tutorial/VogellaSpringbootPlaygroundTutorialAndeApplicationTests.java
@@ -0,0 +1,13 @@
+package at.ande.vogella.springboot.playground.tutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VogellaSpringbootPlaygroundTutorialAndeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
